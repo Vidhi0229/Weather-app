abb77d74e703a99d05962312ec9cbfcfd7fc509d
Vidhi <srivastavavidhi29@gmail.com>
2023-08-28 22:02:34 +0530

    Get city api call

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,1 +1,16 @@
 const key = 'DtOGI2V97Phiyg8ecyzvAv1Ap0HtAEp2';
+
+const getCity = async(city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query) 
+    const data = await response.json()
+
+    console.log(data[0])
+    return data[0]
+}
+
+getCity("Lucknow")
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
