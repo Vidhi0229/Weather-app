a775df428c42cfc581aa02be4e8def9d5c6b2192
Vidhi <srivastavavidhi29@gmail.com>
2023-09-03 19:56:34 +0530

    encapsulating all function in one class

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,27 +1,55 @@
-const key = 'spGEfKmbxWJpSqe8ShOfHHqoNmziP1jb';
+class Forecast{
+    constructor(){
+        this.key = 'spGEfKmbxWJpSqe8ShOfHHqoNmziP1jb';
+        this.WeatherBase = 'https://dataservice.accuweather.com/currentconditions/v1/';
+        this.CityBase = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+    }
 
-const getWeather = async(id) => {
-    const base = 'https://dataservice.accuweather.com/currentconditions/v1/'
-    const query = `${id}?apikey=${key}`
+    async updateCity(city){
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return {cityDets, weather};
+    }
 
-    const response = await fetch(base + query)
-    const data = await response.json()
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`
+        const response = await fetch(this.CityBase + query)
+        const data = await response.json()
+        return data[0]
+    }
 
-    //console.log(data[0])
-
-    return data[0]
+    async getWeather(id){
+        const query = `${id}?apikey=${key}`
+        const response = await fetch(this.WeatherBase + query)
+        const data = await response.json()
+        return data[0]
+    }
 }
 
-const getCity = async(city) => {
-    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?apikey=${key}&q=${city}`
+// const key = 'spGEfKmbxWJpSqe8ShOfHHqoNmziP1jb';
 
-    const response = await fetch(base + query) 
-    const data = await response.json()
+// const getWeather = async(id) => {
+//     const base = https://dataservice.accuweather.com/currentconditions/v1/'
+//     const query = `${id}?apikey=${key}`
 
-    //console.log(data[0])
-    return data[0]
-}
+//     const response = await fetch(base + query)
+//     const data = await response.json()
+
+//     //console.log(data[0])
+
+//     return data[0]
+// }
+
+// const getCity = async(city) => {
+//     const base = 'https://dataservice.accuweather.com/locations/v1/cities/search'
+//     const query = `?apikey=${key}&q=${city}`
+
+//     const response = await fetch(base + query) 
+//     const data = await response.json()
+
+//     //console.log(data[0])
+//     return data[0]
+// }
 
 // getCity("California")
 //     .then(data => {
